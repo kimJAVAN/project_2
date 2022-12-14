ecc8405729b7ba360fc841834826f66ab290eef5
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2022-12-14 16:20:43 +0900

    2022.12.14-5

--- js/common.js
+++ js/common.js
@@ -4,29 +4,6 @@ function openNav(){
 function closeNav(){
     document.getElementById("myNav").style.width="0%";}
 
-
-$(function(){
-    var page = 0;
-    $('.prev').click(function () {
-        page--;
-        if (page < 0) {
-            page = 0;
-            alert('first member');
-            return;
-        }
-        $('.slide').stop().animate({ marginLeft: -300 * page }, 1600);
-        });
-    $('.next').click(function () {
-        page++;
-        if (page > $('.slide li').length-2) {
-            page = $('.slide li').length-2;
-            alert('last member');
-            return;
-        }
-        $('.slide').stop().animate({ marginLeft: -300 * page }, 1600);
-        });
-    });
-
 $(function(){
     $("#button1_1").click(function(){
     $("#slide1_2").fadeIn();
