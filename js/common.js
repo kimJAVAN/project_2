d149a1dcd41d6378c4580699a0b7ad2b1b9dc4e9
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2022-12-14 17:57:44 +0900

    2022.12.14 last

--- js/common.js
+++ js/common.js
@@ -14,3 +14,31 @@ $(function(){
     $("#slide1_2").hide();
 })
 });
+
+$(function(){
+    var index=0;
+    $(`.slide li:nth-of-type(${index+2})`).css('transform','scale(1.2)').css('transition','all 1s ease');
+    $('.prev').click(function () {
+        index--;
+        if (index < 0) {
+            index = 0;
+            alert('First Member');
+            return;
+        }
+        $('.slideul').stop().animate({ marginLeft: -336 * index }, 800);
+        $('.slideul li').css('transform','scale(1)');
+        $(`.slideul li:nth-of-type(${index+2})`).css('transform','scale(1.2)').css('transition','all 1s ease');
+    });
+    $('.next').click(function () {
+
+        index++;
+        if (index > $('.slideul li').length-3) {
+            index = $('.slideul li').length-3;
+            alert('Last Member');
+            return;
+        }
+        $('.slideul').stop().animate({ marginLeft: -336 * index }, 800);
+        $('.slideul li').css('transform','scale(1)');
+        $(`.slideul li:nth-of-type(${index+2})`).css('transform','scale(1.2)').css('transition','all 1s ease');
+    });
+})
